f48bc56696c6b81268959356b7b43a588b77a147
oviedojhorjan924-maker <oviedojhorjan924@gmail.com>
2026-03-13 23:22:19 -0500

    Calculadora_promedio.py
    The program calculates the average of three grades and checks if the student passed.

---
+++ calculadora_promedio.py
@@ -0,0 +1,49 @@
+#ALGORITMO
+#1. Pedir la Nota1 al usuario
+#2. Pedir la Nota2 al usuario
+#3. Pedir la Nota3 al usuario
+#4. Mostrar Nota1
+#5. Mostrar Nota2
+#6. Mostrar Nota3
+#7. Calcular el promedio (Nota1 + Nota2 + Nota3) /3
+#8. Mostrar Promedio 
+#9. Comparar si el estudiante aprobó (Nota minima 3.0)
+#10. Mostrar el resultado True o False
+
+#PSEUDOCODIGO 
+#INICIO 
+    #LEER Nota1
+    #LEER Nota2
+    #LEER Nota3
+    #Promedio<-(Nota1 + Nota2 + Nota3) /3
+
+    #ESCRIBIR Nota1
+    #ESCRIBIR Nota2
+    #ESCRIBIR Nota3
+    #ESCRIBIR Promedio
+    #ESCRIBIR  "¿El estudiante aprobó?", Promedio >= 3.0
+#FIN
+
+Nota1 =float(input("Ingrese la nota 1: "))
+Nota2 =float(input("Ingrese la nota 2: "))
+Nota3 =float(input("Ingrese la nota 3: "))
+
+Promedio = (Nota1 + Nota2 + Nota3) /3
+
+print("La nota 1 es: ", Nota1)
+print("La nota 2 es: ", Nota2)
+print("La nota 3 es: ", Nota3)
+print("El promedio fue: ", Promedio)
+print("¿El estudiante aprobó?: ", Promedio >= 3.0)
+
+Nota1 =float(input("Ingrese la nota 1: "))
+Nota2 =float(input("Ingrese la nota 2: "))
+Nota3 =float(input("Ingrese la nota 3: "))
+
+Promedio = (Nota1 + Nota2 + Nota3) /3
+
+print("La nota 1 es: ", Nota1)
+print("La nota 2 es: ", Nota2)
+print("La nota 3 es: ", Nota3)
+print("El promedio fue: ", Promedio)
+print("¿El estudiante aprobó?: ", Promedio >= 3.0)
